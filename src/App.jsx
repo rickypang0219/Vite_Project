4ce4ee378168e652347c63c7adf98306c923ebed
Ricky_Pang <60353958+rickypang0219@users.noreply.github.com>
2023-07-28 22:34:58 +0800

    Original Features

--- src/App.jsx
+++ src/App.jsx
@@ -7,126 +7,50 @@ import { useTheme, ThemeProvider, createTheme } from '@mui/material/styles';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
 import useMediaQuery from '@mui/material/useMediaQuery';
-import TestGrid from './testing/TestGrid'
 
 
-// function App() {
-//   return (
-//     <Box sx={{ flexGrow: 1 }} >
-//       {/* <Paper style={{ width: '100vw', height: '100vh' }} /> */}
-//       <Grid container style={{ height: '100vh', width: '100vw' }} >
-//         <Grid item xs={12}>
-//           <TestBar />
-//         </Grid>
-//         <Home />
-//       </Grid>
-//     </Box>
-//   );
-// }
-
-// export default App;
-
-
-
-// export default function App() {
-//   return (
-//     <>
-//       <h1> Hello World</h1>
-//     </>
-//   )
-// }
-
+// Correct Code 
 
 const ColorModeContext = React.createContext({ toggleColorMode: () => { } });
 
-// export default function ToggleColorMode() {
-//   const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
-//   const [mode, setMode] = React.useState('light');
-//   const colorMode = React.useMemo(
-//     () => ({
-//       toggleColorMode: () => {
-//         setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
-//       },
-//     }),
-//     [],
-//   );
+export default function ToggleColorMode() {
+  const [wrappedScreen, setWrappedScreen] = React.useState(null);
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)', { noSsr: true });
 
-//   const theme = React.useMemo(
-//     () =>
-//       createTheme({
-//         palette: {
-//           mode,
-//         },
-//       }),
-//     [mode],
-//   );
+  const [mode, setMode] = React.useState(prefersDarkMode ? 'dark' : 'light');
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+      },
+    }),
+    [],
+  );
 
-//   return (
-//     <ColorModeContext.Provider value={colorMode}>
-//       <ThemeProvider theme={theme}>
-//         <CssBaseline />
-//           <Grid container style={{width: '100vw' }} >
-//             <Grid item xs={12}>
-//               <Navbar colorMode={colorMode} theme={theme} />
-//             </Grid>
-//             <Grid item>
-//               <Home theme ={theme} />
-//             </Grid>
-//           </Grid>
-//       </ThemeProvider>
-//     </ColorModeContext.Provider>
-//   );
-// }
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode],
+  );
 
-
-// export default function ToggleColorMode() {
-//   const [wrappedScreen, setWrappedScreen] = React.useState(null);
-//   const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)', { noSsr: true });
-//
-//   const [mode, setMode] = React.useState(prefersDarkMode ? 'dark' : 'light');
-//   const colorMode = React.useMemo(
-//     () => ({
-//       toggleColorMode: () => {
-//         setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
-//       },
-//     }),
-//     [],
-//   );
-//
-//   const theme = React.useMemo(
-//     () =>
-//       createTheme({
-//         palette: {
-//           mode,
-//         },
-//       }),
-//     [mode],
-//   );
-//
-//   return (
-//     <ColorModeContext.Provider value={colorMode}>
-//       <ThemeProvider theme={theme}>
-//         <CssBaseline />
-//         <Grid container style={{ width: '100vw' }} >
-//           <Grid item xs={12}>
-//             <Navbar colorMode={colorMode} theme={theme} />
-//           </Grid>
-//           <Box>
-//           <Home theme={theme} />
-//           </Box>
-//         </Grid>
-//       </ThemeProvider>
-//     </ColorModeContext.Provider>
-//   );
-// }
-
-
-
-
-export default function App () {
-  return ( 
-  <> 
-      <TestGrid />
-  </>
-  )
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Grid container style={{ width: '100vw' }} >
+          <Grid item xs={12}>
+            <Navbar colorMode={colorMode} theme={theme} />
+          </Grid>
+          <Box>
+          <Home theme={theme} />
+          </Box>
+        </Grid>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
+
---
+++ src/testing/HomeTest.jsx
@@ -0,0 +1,65 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import { Avatar, Typography } from '@mui/material';
+// import Para from './Para'
+
+
+export default function FullWidthGrid() {
+  return (
+    <Box sx={{ flexGrow: 1,
+      position:'relative',
+    height:'100vh'}}>
+      <Grid container rowSpacing={2} columnSpacing={1} width='100vw' style={{height:'100%', position:'absolute'}}  >
+        <Grid item xs={12} > 
+          <Typography style={{textAlign:'center'}}> Navbar </Typography>
+        </Grid>
+        <Grid item xs={12} md={4} style={{ position:'relative' }} >
+          <Box  sx={{ height:'100vh' , width:'100%', justifyContent:'center', alignItems:'center', display:'flex', textAlign:'center', flexDirection:'column' }}>
+            <Avatar sx={{ height:150, width:150 }} alt="Ricky"> </Avatar>
+            <br/>
+            <Typography> Ricky Pang </Typography>
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={8} >
+            <Box sx={{ overflow: 'auto' }}> 
+              <Typography  >
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. 
+                <br/>
+                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. 
+                <br/>
+                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. 
+                <br/>
+                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. 
+                <br/>
+                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. 
+                <br/>
+                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. 
+                <br/>
+                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. 
+                <br/>
+                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. 
+                <br/>
+                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. 
+                <br/>
+     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. 
+                <br/>
+                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. 
+                <br/>
+                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. 
+                <br/>
+              </Typography>
+            </Box>
+        </Grid>
+        {/* <Grid item xs={6} md={4}> */}
+        {/*    <Item>xs=6 md=4</Item> */}
+        {/*  </Grid> */}
+        {/*   <Grid item xs={6} md={8}> */}
+        {/*     <Item>xs=6 md=8</Item> */}
+        {/* </Grid> */}
+      </Grid>
+    </Box>
+  );
+}
